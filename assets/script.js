42cd5d0af3c7ce8501aff56070fceee689dec2e2
jsedlak146 <jssedlak146@gmail.com>
2022-12-07 18:54:52 -0500

    javascript is retrieving data from API

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
+var citySearch = document.querySelector('.city-input');
+// var weather = document.querySelector('');
+var inputForm = document.querySelector('.inputForm');
+var card = document.querySelectorAll('.card');
+// console.log(card[0].children[1].children);  use these to navigate and then line 26 to display inside the boxes
+// card[0].children[1].children[3].innerHTML = "frank Sinatra"
+var apiKey = '6bc39d38aebd579551385cf995faaa5f';
 
+inputForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    fiveDay(citySearch.value);
+    console.log(citySearch.value);
+});
 
-
+function fiveDay(cityName) {
+    var units = 'imperial';
+    var lang = 'en';
+    console.log(cityName)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`).then(function(responseBody) {
+        console.log(responseBody)
+        return responseBody.json();
+    }).then(function(fiveData) {
+        console.log(fiveData)
+        for (let i = 0; i < 5; i++) {
+            console.log(card[i])
+            console.log(fiveData.list[(i * 8)])
+        }
+     })
+;}
 
 
 
